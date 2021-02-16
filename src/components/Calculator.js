24ba01b4da42d161fca82876b29050481d05232c
Pernilla <coolanilla@hotmail.com>
2021-02-16 13:41:20 +0100

    Calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import style from '../css/Calculator.module.css';
+
+function Calculator() {
+
+    return (
+        <div className={style.calculator}>
+            
+        </div>
+    )
+
+}
+
+export default Calculator;
