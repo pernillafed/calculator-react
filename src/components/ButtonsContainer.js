6b988f8ac2d9af8882ddc6a460b41e088834c87d
Pernilla <coolanilla@hotmail.com>
2021-02-16 14:41:07 +0100

    Created buttons data and looped out

--- src/components/ButtonsContainer.js
+++ src/components/ButtonsContainer.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+
 import style from '../css/ButtonsContainer.module.css';
 
 function ButtonsContainer() {
 
+    const [buttonsValues, setButtonsValues] = useState([
+        7, 8, 9, "/", 4, 5, 6, "x", 1, 2, 3, "-", "clear", 0, "+", "="
+    ])
+
     return (
         <div className={style.buttons}>
-            <p>The buttons</p>
+            {buttonsValues.map((value) => (
+                <p key={value}>{value}</p>
+            ))}
         </div>
     )
 
