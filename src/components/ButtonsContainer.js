bd3560ff77c7c0ae397200fce9a5a0cac9b71fcc
Pernilla <coolanilla@hotmail.com>
2021-02-16 15:37:22 +0100

    Updated button container

--- src/components/ButtonsContainer.js
+++ src/components/ButtonsContainer.js
@@ -1,8 +1,5 @@
 import { useState } from 'react';
-<<<<<<< HEAD
 import Button from './Button';
-=======
->>>>>>> dev
 
 import style from '../css/ButtonsContainer.module.css';
 
@@ -10,19 +7,12 @@ function ButtonsContainer() {
 
     const [buttonsValues, setButtonsValues] = useState([
         7, 8, 9, "/", 4, 5, 6, "x", 1, 2, 3, "-", "clear", 0, "+", "="
-<<<<<<< HEAD
     ]);
-=======
-    ])
->>>>>>> dev
 
     return (
         <div className={style.buttons}>
             {buttonsValues.map((value) => (
-<<<<<<< HEAD
                 <Button key={value} />
-=======
->>>>>>> dev
             ))}
         </div>
     )
